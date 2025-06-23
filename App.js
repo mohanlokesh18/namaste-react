d8aa106aee554a3d6a2a65ee7b6c6eadce2205ee
Mohan-Lokesh <mohan.l@visionetsystems.com>
2025-06-23 19:57:58 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-        // const heading = React.createElement('h1',{id:"heading"},"Radhe Radhe! from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+       
+       // const heading = React.createElement('h1',{id:"heading"},"Radhe Radhe! from React");
         // const root = ReactDOM.createRoot(document.getElementById('root'));
         // root.render(heading);
 
@@ -14,8 +17,8 @@
     const element = React.createElement('div',{id:"parent"},
         React.createElement('div',{id:"child"},
             [
-                React.createElement('h1',{},"I'm h1 tag"),
-                React.createElement('h2',{},"I'm h2 tag")
+                React.createElement('h1',{key:"h1"},"I'm h1 tag"),
+                React.createElement('h2',{key:"h2"},"I'm h2 tag")
             ]))
 
     const root = ReactDOM.createRoot(document.getElementById('root'));
